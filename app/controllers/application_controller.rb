a50d356942b2ba4e8c0cf1c8b6757a7ee4d7b7b9
carrie nye <cnye@chicagobooth.edu>
2023-04-09 20:15:18 +0000

    RPS v1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,16 @@ def play_rock
   render ({ :template => "game_templates/user_rock.html.erb"})
 end
 
+def homepage
+  render ({ :template => "game_templates/rules.html.erb"})
+end
+
+def play_paper
+  render ({ :template => "game_templates/user_paper.html.erb"})
+end
+
+def play_scissors
+  render ({ :template => "game_templates/user_scissors.html.erb"})
+end
+
 end
